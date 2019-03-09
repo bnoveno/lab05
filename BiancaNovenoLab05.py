372755448fa8f4a2e1a07b79153240c35ac5a021
bianca noveno <novenobianca@gmail.com>
2019-03-08 19:34:14 -0500

    changed labels and added comments

--- BiancaNovenoLab05.py
+++ BiancaNovenoLab05.py
@@ -4,8 +4,10 @@ Lab 5: Conditional Statements
 Using conditional statements to plot random points in different quadrats
 Due date: March 15, 2019
 """
+#import modules
 import matplotlib.pyplot as plt
 import random
+
 #set up the rectangle
 min_long = -109
 max_long = -102
@@ -17,14 +19,15 @@ bp_long = -105.5
 bp_lat = 39
 
 #5 points for the rectangle: (min_long,min_lat), (max_long,min_lat), (max_long, max_lat), (min_long, max_lat), (min_long, min_lat)
+#make 2 lists with the corresponding coordinates for the rectangle
 longitude = [min_long, max_long, max_long, min_long, min_long]
 latitude = [min_lat, min_lat, max_lat, max_lat, min_lat]
 
 #plot the rectangle
 plt.plot(longitude, latitude)
 #include axis labels
-plt.xlabel = ('Longitude')
-plt.ylabel = ('Latitude')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
 
 for i in range (0,500):
     rand_long = random.uniform(min_long, max_long)
@@ -40,6 +43,6 @@ for i in range (0,500):
             symbol = 'gs'
         else: #lower left
             symbol = 'r*'
-    plt.plot(rand_long, rand_lat, symbol)    
-plt.show()
-raw_input ("press any key to show the results")
+    plt.plot(rand_long, rand_lat, symbol) #plot the random longitude and latitude with corresponding symbol 
+plt.show() #shows plot
+raw_input ("press any key to show the results") 
